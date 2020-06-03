c2724f25b0282a6507a189674521297144a0b4c5
chenrenfei <102309713@qq.com>
2020-06-03 13:47:08 +0800

    Update StaffInfoVO.java

--- trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/demo/vo/StaffInfoVO.java
+++ trunk/sqltoy-orm/src/test/java/org/sagacity/sqltoy/demo/vo/StaffInfoVO.java
@@ -5,16 +5,25 @@ package org.sagacity.sqltoy.demo.vo;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author zhong
  *
  */
+@Entity
+@Table(name = "", schema = "")
 public class StaffInfoVO implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5297979866915758170L;
+
+	@Id
+	private String staffId;
 
 	private String staffName;
 
